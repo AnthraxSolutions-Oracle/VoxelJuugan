f24f80a76cafbd9f0b77b9deafb7fce95454fde4
unknown <enzo.rugai@20130143E6A3.curric.gihs.sa.edu.au>
2014-12-10 19:27:49 +1030

    Working On it

---
+++ Project/Assets/Code/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = Input.mousePosition;
+		pos.z = 20;
+		Debug.Log (pos.x + " " + pos.y + " " + pos.z + " ");
+		pos = Camera.main.ScreenToWorldPoint(pos);
+		pos.y =0;
+		transform.position = pos;
+	}
+}
